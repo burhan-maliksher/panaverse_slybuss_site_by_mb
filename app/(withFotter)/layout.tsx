b9a7fe7cbcbcc9b06cebea8b793c29ad97d6afb9
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2024-06-24 11:52:43 +0500

    Update layout.tsx

--- app/(withFotter)/layout.tsx
+++ app/(withFotter)/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import { Inder } from 'next/font/google'
 import Nav from './nav';
 import Footer from './footer';
-import Script from 'next/script';
 
 export const metadata = {
   title: 'Panaverse Syllabus Site by MB',
@@ -24,26 +23,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${inder.variable}`}>
-      
-      <head>
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
-        />
-        
-        <Script id="ga-script" strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-                `}
-        </Script>
-      </head>
-      
+    <html lang="en" className={`${inder.variable}`}>      
       <body>
         <Nav/>
         {children}
